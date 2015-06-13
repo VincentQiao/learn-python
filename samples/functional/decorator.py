b1e5149e13527a6b58469cd6e05d6c390eb7dfe3
vincent <tqiao@kalengo.com>
2015-06-14 00:14:56 +0800

    decorator

--- samples/functional/decorator.py
+++ samples/functional/decorator.py
@@ -2,15 +2,62 @@
 __author__ = 'Vincent'
 
 import time
+import functools
 
-def log(func):
+
+def log_simple(func):
+    @functools.wraps(func)
     def wrapper(*args, **kw):
         print('call %s():' % func.__name__)
         return func(*args, **kw)
     return wrapper
 
-@log
+
+@log_simple
 def now():
     print(time.strftime('%Y-%m-%d %H:%I:%M', time.localtime(time.time())))
 
+
+def log_text(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s()' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+@log_text('hello')
+def new_now():
+    print(time.strftime('%Y-%m-%d %H:%I:%M', time.localtime(time.time())))
+
+
+def log(arg):
+    # arg is not text, it's a func name
+    if not isinstance(arg, str):
+        @functools.wraps(arg)
+        def wrapper(*args, **kw):
+            print('call %s()' % arg.__name__)
+            return arg(*args, **kw)
+        return wrapper
+    # arg is text
+    else:
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kw):
+                print('%s %s()' % (arg, func.__name__))
+                return func(*args, **kw)
+            return wrapper
+        return decorator
+
+
+@log
+def f1():
+    pass
+
+
+@log('hello')
+def f2():
+    pass
 
