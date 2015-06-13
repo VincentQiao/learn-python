9f49a41fa45249c3b3b260c0eb39da1407cda878
vincent <tqiao@kalengo.com>
2015-06-14 02:07:39 +0800

    student

--- py3/oop_basic/student.py
+++ py3/oop_basic/student.py
@@ -3,6 +3,11 @@ __author__ = 'vincent'
 
 
 class Student(object):
+
     def __init__(self, name):
         self.name = name
+        self.gender = None
+
+    def gender(self, gender):
+        self.gender = gender
 
