11d468cf854c33e8e3a5379e726c7dc05dab7dca
Vincent <qiao_tianjian@163.com>
2015-06-30 13:13:24 +0800

    datetime

--- py3/commonlib/use_datetime.py
+++ py3/commonlib/use_datetime.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Vincent'
 
+import re
 from datetime import datetime, timedelta, timezone
 
 # 获得当前时间的datetime
@@ -37,6 +38,33 @@ print('now + 3 days, 2 hours', now+timedelta(days=3, hours=2))
 
 # 本地时间转换为UTC时间
 tz_utc_8 = timezone(timedelta(hours=8))
+now = datetime.now()
+dt = now.replace(tzinfo=tz_utc_8)
+print('now:', now)
+print('time utc-8:', dt)
+
+# 时区转换
+utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+print('utc-dt:', utc_dt)
+bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
+print('bj_dt:', bj_dt)
+tokyo_dt = utc_dt.astimezone(timezone(timedelta(hours=9)))
+tokyo2_dt = bj_dt.astimezone(timezone(timedelta(hours=9)))
+print('tokyo_dt:', tokyo_dt)
+print('tokyo_dt2:', tokyo_dt)
 
 
+def to_timestamp(dt_str, tz_str):
+    hour = int(re.compile(r'UTC(.\d+?):').findall(tz_str)[0])
+    here_dt = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
+    out_dt = here_dt.replace(tzinfo=timezone(timedelta(hours=hour)))
+    return out_dt.timestamp()
 
+# 测试:
+t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+assert t1 == 1433121030.0, t1
+
+t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
+assert t2 == 1433121030.0, t2
+
+print('Pass')
