af1275cfb195ba45a6171f81f215b0ac50b7faca
vincent <tqiao@kalengo.com>
2015-07-07 23:36:43 +0800

    use_itertools

--- py3/commonlib/use_itertools.py
+++ py3/commonlib/use_itertools.py
@@ -2,4 +2,34 @@
 __author__ = 'Vincent'
 
 import itertools
+naturals = itertools.count(1)
+i = 0
+for n in naturals:
+    i += 1
+    if i > 4:
+        break
+    print(n)
 
+
+cs = itertools.cycle('ABC')
+
+for c in cs:
+    i += 1
+    if i > 10:
+        break
+    print(c)
+
+ns = itertools.repeat('A', 3)
+for n in ns:
+    print(n)
+
+new_naturals = itertools.count(1)
+ns = itertools.takewhile(lambda x: x <= 10, new_naturals)
+print(list(ns))
+
+for c in itertools.chain('ABC', 'XYZ'):
+    print(c)
+
+# groupby, 把迭代器中相邻的重复元素挑出来
+for key, group in itertools.groupby('AAABBBCCAAA'):
+    print(key, list(group))
