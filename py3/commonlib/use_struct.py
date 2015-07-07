74808ee126dd402d9a51827ba6f16e3ec2d566e4
vincent <tqiao@kalengo.com>
2015-07-07 23:46:38 +0800

    use_xml_sax

--- py3/commonlib/use_struct.py
+++ py3/commonlib/use_struct.py
@@ -4,5 +4,5 @@ __author__ = 'Vincent'
 import struct
 struct.pack('>I', 10240099)
 struct.pack('>I', 4042322160)
-struct.unpack('>I',struct.pack('>I', 10240099))
+struct.unpack('>I', struct.pack('>I', 10240099))
 print(struct.unpack('>IIH', b'\x00\x9c@c\xf0\xf0\xf0\xf0\x80\x80'))
